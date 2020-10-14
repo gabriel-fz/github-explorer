70a3a0209e0bc14482cd917193c0ff316b53d387
gabriel-fz <gabrielfzancanela@gmail.com>
2020-10-14 14:58:26 -0300

    Estilizaçao do dashboard concluido

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/15328480?s=460&u=62217625331a1c62660099599f8af32f16e0eecc&v=4"
+            alt="Gabriel Fiorese"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Uma breve descrição aqui</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/15328480?s=460&u=62217625331a1c62660099599f8af32f16e0eecc&v=4"
+            alt="Gabriel Fiorese"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Uma breve descrição aqui</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/15328480?s=460&u=62217625331a1c62660099599f8af32f16e0eecc&v=4"
+            alt="Gabriel Fiorese"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Uma breve descrição aqui</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
